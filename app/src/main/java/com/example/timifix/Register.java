bd11865513c7764092974466669e2d1cdf665301
Fuziune <ioan.fusarau@stud.ubbcluj.ro>
2024-04-06 19:46:35 +0300

    yy7

--- app/src/main/java/com/example/timifix/Register.java
+++ app/src/main/java/com/example/timifix/Register.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -27,19 +28,24 @@ public class Register extends AppCompatActivity {
     TextInputEditText editTextEmail,editTextPassword;
     Button buttonReg;
     FirebaseAuth mAuth;
+
+    ProgressBar progressBar;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        auth= FirebaseAuth.getInstance();
+        mAuth= FirebaseAuth.getInstance();
+
         setContentView(R.layout.activity_register);
         editTextEmail=findViewById(R.id.email);
         editTextPassword=findViewById(R.id.password);
         buttonReg=findViewById(R.id.btn_register);
+        progressBar.findViewById(R.id.progressBar);
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email,password;
                 email=String.valueOf(editTextEmail.getText());
                 password=String.valueOf(editTextPassword.getText());
@@ -57,11 +63,12 @@ public class Register extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(Register.this, "Account created.",
                                             Toast.LENGTH_SHORT).show();
                                 } else {
                                     // If sign in fails, display a message to the user.
-
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(Register.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
 
