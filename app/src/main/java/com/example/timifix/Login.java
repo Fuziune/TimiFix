06a9681416e474d2998d2a6954da6ce45a02da99
VasiAlexandru <tk60726@gmail.com>
2024-04-07 19:46:23 +0300

    Pagina de profil partiala

--- app/src/main/java/com/example/timifix/Login.java
+++ app/src/main/java/com/example/timifix/Login.java
@@ -1,6 +1,10 @@
 package com.example.timifix;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Login extends AppCompatActivity {
+    public TextView loginTextView;
+    public TextInputLayout email;
+    public TextInputLayout password;
+    public Button loginButton;
+    public Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,9 @@ public class Login extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void moveToRegister(View view){
+        Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
     }
 }
